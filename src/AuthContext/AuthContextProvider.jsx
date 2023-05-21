aaaf20d93b3a40b6e91dcdaa3f1837969bb34740
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-05-21 17:19:46 +0530

    added context api fixed

--- src/AuthContext/AuthContextProvider.jsx
+++ src/AuthContext/AuthContextProvider.jsx
@@ -1,9 +1,16 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuth, setisAuth] = useState(false);
+  const [isAuth, setisAuth] = useState(() => {
+    const storedIsAuth = JSON.parse(localStorage.getItem("isAuth"));
+    return storedIsAuth ? storedIsAuth : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("isAuth", JSON.stringify(isAuth));
+  }, [isAuth]);
 
   const loginFunction = () => {
     setisAuth(true);
